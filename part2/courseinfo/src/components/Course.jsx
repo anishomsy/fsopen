21702c921a46f520d767e68b28e9c8970bd170b3
Anis Homsy <anisfhomsi@gmail.com>
2024-08-16 21:40:27 +0300

    format using prettier

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,39 +1,39 @@
 function Course({ course }) {
-  return (<>
-    <Header text={course.name} />
-    <Content parts={course.parts} />
-
-
-
-  </>);
+  return (
+    <>
+      <Header text={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
 }
 
-
 export function Header({ text }) {
-  return (<h1>{text}</h1>);
+  return <h1>{text}</h1>;
 }
 
 export function Content({ parts }) {
   const total = parts.reduce((sum, part) => {
-    return sum + part.exercises
+    return sum + part.exercises;
   }, 0);
 
-  return (<>
-
-    {
-      parts.map((part) => {
-        return (<Part key={part.id} part={part} />);
-      })
-
-    }
-    <p><strong>total of {total} exercises</strong></p>
-
-
-  </>);
+  return (
+    <>
+      {parts.map((part) => {
+        return <Part key={part.id} part={part} />;
+      })}
+      <p>
+        <strong>total of {total} exercises</strong>
+      </p>
+    </>
+  );
 }
 
 export function Part({ part }) {
-  return (<p>{part.name} {part.exercises}</p>);
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
 }
 
 export default Course;
