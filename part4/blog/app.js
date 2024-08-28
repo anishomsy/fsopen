cdc0e7693a7f3359b92bb83603a213b088e08d98
Anis Homsy <anisfhomsi@gmail.com>
2024-08-28 13:34:52 +0300

    complete refactoring. seperate index.js into seperate parts for eaiser managment

---
+++ part4/blog/app.js
@@ -0,0 +1,30 @@
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const express = require("express");
+
+const cors = require("cors");
+const blogRouter = require("./controllers/blog");
+
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+// config express
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/blog", blogRouter);
+
+module.exports = app;
